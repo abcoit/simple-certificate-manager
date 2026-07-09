4f0e8ff0e1b94052369a1cdfbb7a02d3514566d6
abcoit <abco.it@abcoresearch.com>
2026-07-09 14:14:00 -0500

    Fix certificate import command with keyfile password
    Still working on decryption issue with keyfile.

--- simple-certificate-manager.sh
+++ simple-certificate-manager.sh
@@ -625,9 +625,7 @@ import_certificate() {
     chmod 600 "$key_file"
     
     # Import certificate
-    # Having issues with keyfile password. Temporarily comment out next line and rewrite new line for testing.
-    # if fmsadmin certificate import "$cert_file" --keyfile "$key_file" -y -u "$FMS_USERNAME" -p "$FMS_PASSWORD" >> "$FMS_LOG_PATH/fms-import.log" 2>&1; then
-    if fmsadmin certificate import "$cert_file" --keyfile "$key_file" -y -u "$FMS_USERNAME" -p "" >> "$FMS_LOG_PATH/fms-import.log" 2>&1; then
+    if fmsadmin certificate import "$cert_file" --keyfile "$key_file" --keyfilepass "$FMS_PASSWORD" -y -u "$FMS_USERNAME" -p "$FMS_PASSWORD" >> "$FMS_LOG_PATH/fms-import.log" 2>&1; then
         log_success "Certificate imported successfully"
         return 0
     else
