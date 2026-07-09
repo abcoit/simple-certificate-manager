eaf744d1dd014d140eae3659aa38a9156c0a9ee5
abcoit <abco.it@abcoresearch.com>
2026-07-09 14:38:03 -0500

    Remove unused keyfile password from import command
    Issue with import found. Outside of scipt. Reverted chages.

--- simple-certificate-manager.sh
+++ simple-certificate-manager.sh
@@ -625,7 +625,7 @@ import_certificate() {
     chmod 600 "$key_file"
     
     # Import certificate
-    if fmsadmin certificate import "$cert_file" --keyfile "$key_file" --keyfilepass "" -y -u "$FMS_USERNAME" -p "$FMS_PASSWORD" >> "$FMS_LOG_PATH/fms-import.log" 2>&1; then
+    if fmsadmin certificate import "$cert_file" --keyfile "$key_file" -y -u "$FMS_USERNAME" -p "$FMS_PASSWORD" >> "$FMS_LOG_PATH/fms-import.log" 2>&1; then
         log_success "Certificate imported successfully"
         return 0
     else
